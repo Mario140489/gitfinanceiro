e2340ae6e780f34ca3247c5cffc103a2102ff562
jmario140489 <jm_bck@hotmail.com>
2018-06-08 16:27:24 -0300

    certo

---
+++ Financeiro/Models/tp_conta.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Financeiro.Models
+{
+    public class tp_conta
+    {
+        [Key]
+        public int idtp_conta { get; set; }
+        [RegularExpression(@"^[a-zA-Z]+$", ErrorMessage = "Use apenas caracteres alfabéticos.")]
+        [StringLength(100, MinimumLength = 10, ErrorMessage = "Descrição deve conter mínimo de 10 e máximo 100 caracteres!")]
+        [Required(ErrorMessage = "Campo obrigatório!")]
+        [Display(Name ="Tipo de conta")]
+        public string descricao { get; set; }
+        public bool fundos { get; set; }
+        public bool investimento { get; set; }
+    }
+}
---
+++ famafin/Financeiro/Models/bancos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Financeiro.Models
+{
+    public class bancos
+    {
+        [Key]
+        public string bancos_id { get; set; }
+        [Display(Name ="Banco")]
+        public string descricao { get; set; }
+    }
+}
